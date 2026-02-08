2d18342b242c6076d5d4fdd5c3f896847cc8c2ee
m9china <nqobilencaza2@gmail.com>
2026-02-08 19:00:25 +0200

    feat: adjust padding in Home component for improved layout consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { CTA, FeaturedProjects, Footer, Hero, NavBar, TechStack } from "@/compon
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center font-sans">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-8 bg-[#0d1117] sm:items-start">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between pt-32 px-8 bg-[#0d1117] sm:items-start">
         <NavBar/>
         <Hero/>
         <FeaturedProjects/>
