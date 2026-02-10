57e4040510905b852767f4215c2f8e370afbbc89
m9china <nqobilencaza2@gmail.com>
2026-02-10 17:46:06 +0200

    feat: simplify project description on Projects page for clarity

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,7 +5,7 @@ export default function Projects() {
     <main className="container min-h-screen px-8 pt-32 py-16 bg-[#0d1117] w-full">
       <h1 className="text-3xl font-bold mb-8 text-[#c9d1d9] font-semibold">{`These are some of the projects I've worked on`}</h1>
       <p className="text-lg text-[#c9d1d9] mb-12">
-        {`Here are some of the projects I've worked on. Click on each project to learn more about it.`}
+        {`Click on each project to learn more about it.`}
       </p>
       <div className="grid md:grid-cols-2 gap-8">
         <ProjectCard />
